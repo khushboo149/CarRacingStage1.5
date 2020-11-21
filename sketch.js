d0f41d2690dcc663a2e2ea54de54a11ce1a532d5
khushboo149 <71179638+khushboo149@users.noreply.github.com>
2020-11-21 18:29:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+function preload(){
+  track=loadImage("./images/track.jpg");
+  car1_img=loadImage("./images/car1.png");
+  car2_img=loadImage("./images/car2.png");
+  car3_img=loadImage("./images/car3.png");
+  car4_img=loadImage("./images/car4.png");
+  ground=loadImage("./images/ground.png");
 
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
